2f3ff98fdf7a7da9f7f2e1e0f8219733863a2631
Rahultadmybrand <rt040371@gmail.com>
2024-01-24 16:36:26 +0530

    changed imports

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 const router = new Router();
-
 const gh_form = document.querySelector("#gh_form");
-// gh_form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   const username = gh_form["username"].value;
-//   router.route("/repos", () => {});
-// });
